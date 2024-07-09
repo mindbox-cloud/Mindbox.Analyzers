aa31ce50fbbd42e0b702d6da6c1d45712e2e9cc4
Alexander Lipatov <lipatov@mindbox.cloud>
2024-07-09 09:05:45 +0300

    refactor

--- Mindbox.Analyzers/Rules/GroupByEntityRule.cs
+++ Mindbox.Analyzers/Rules/GroupByEntityRule.cs
@@ -8,12 +8,11 @@ namespace MindboxAnalyzers.Rules;
 public class GroupByEntityRule : AnalyzerRule, ISemanticModelAnalyzerRule
 {
 	private const string RuleId = "Mindbox2005";
-	private const string Title =
-		"Forbids grouping by properties of type with [Table] attribute.";
-	private const string MessageFormat =
-		"Grouping by properties of type with [Table] attribute is not allowed.";
+	private const string Title = "Grouping by navigation property check";
+	private const string MessageFormat = "Grouping by navigation properties is not allowed";
 	private const string Description =
-		"Grouping by properties of type with [Table] attribute is not allowed to avoid issues with xmin/xid in Postgres (see https://github.com/npgsql/efcore.pg/issues/3202).";
+		"Grouping by navigation properties is not allowed to avoid issues " +
+		"with xmin/xid in Postgres (see https://github.com/npgsql/efcore.pg/issues/3202)";
 
 	public GroupByEntityRule()
 		: base(
@@ -73,14 +72,18 @@ public class GroupByEntityRule : AnalyzerRule, ISemanticModelAnalyzerRule
 			var propertyTypeSymbol = propertySymbol.Type;
 			var attributes = propertyTypeSymbol.GetAttributes();
 
-			if (attributes.Any(x =>
-				x.AttributeClass is { } attributeClass
-				&& attributeClass.ContainingNamespace.ToString() == "System.ComponentModel.DataAnnotations.Schema"
-				&& attributeClass.Name == "TableAttribute"))
+			var tableAttribute = attributes.FirstOrDefault(x =>
+				x.AttributeClass is not null
+				&& x.AttributeClass.ContainingNamespace.ToString() == "System.ComponentModel.DataAnnotations.Schema"
+				&& x.AttributeClass.Name == "TableAttribute");
+
+			if (tableAttribute is null)
 			{
-				var diagnostic = CreateDiagnosticForLocation(groupByProperty.GetLocation());
-				foundProblems.Add(diagnostic);
+				continue;
 			}
+
+			var diagnostic = CreateDiagnosticForLocation(groupByProperty.GetLocation());
+			foundProblems.Add(diagnostic);
 		}
 	}
 }
